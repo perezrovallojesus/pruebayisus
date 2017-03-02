0a1993172331160d05f000edd0f1b7d5c4468fda
Jesus Manuel Perez Rovallo <jesusm.perez@labcomp.unet.edu.ve>
2017-03-02 15:51:19 -0430

    Este es mi primer commit c:

---
+++ DevC001/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "string.h"	
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	cout << "Esto no se modifica no joda, solo los yisus estan autorizados."
+	system("PAUSE");
+	return 0;
+}
